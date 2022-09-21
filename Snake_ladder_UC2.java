5e90eef7ab9eff6cdd2e8280c64473bc88870bb3
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-21 22:10:13 +0530

    Added:Snake_ladderUC2 logic

---
+++ Snake_ladder_UC2.java
@@ -0,0 +1,11 @@
+class Snake_ladder_UC2 {
+
+	public static void main(String[] args) {
+		final int START_POSITION=0;
+		if(START_POSITION==0)
+			System.out.println("Game Started");
+		int dicenum=(int)Math.floor(Math.random()*6+1);
+		System.out.println("dicenum="+dicenum);
+	}
+
+}
